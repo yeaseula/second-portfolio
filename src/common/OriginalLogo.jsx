a729aaba0e60e31974fb3acac3c3115895f88377
yeaseula <kminea@naver.com>
2026-02-20 15:10:19 +0900

    title logo font style 수정 및 preload 항목 변경

--- src/common/OriginalLogo.jsx
+++ src/common/OriginalLogo.jsx
@@ -1,3 +1,7 @@
 export default function OriginalLogo({ text }) {
-  return <div className="font-bebas text-2xl leading-normal mb-7">{text}</div>;
+  return (
+    <div className="font-pretendard font-bold tracking-tight text-2xl leading-normal mb-7">
+      {text}
+    </div>
+  );
 }
